8939c5850122b74884a8992468f968d8904f1d09
Nahuel <nahuelfaridrosales@gmail.com>
2024-12-26 23:30:27 -0300

    add emailJS to project

--- src/components/sections/contacto/index.jsx
+++ src/components/sections/contacto/index.jsx
@@ -1,7 +1,28 @@
-import Button from "../../../commons/Button";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+/* import Button from "../../../commons/Button"; */
 import "./style.scss";
 
 const Contacto = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_fy0curq", "template_3mtk44g", form.current, {
+        publicKey: "KyYHjzaqfguwNM_Nc",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <section className="contacto">
       <div>
@@ -14,60 +35,14 @@ const Contacto = () => {
         </div>
       </div>
       <div id="after_submit"></div>
-      <form
-        id="contact_form"
-        action="#"
-        method="POST"
-        className="formulario"
-        encType="multipart/form-data"
-      >
-        <div className="row">
-          <label className="required" htmlFor="name">
-            Tu nombre:
-          </label>
-          <input
-            id="name"
-            className="input"
-            name="name"
-            type="text"
-            value=""
-            size="30"
-          />
-
-          <span id="name_validation" className="error_message"></span>
-        </div>
-        <div className="row">
-          <label className="required" htmlFor="email">
-            Your email:
-          </label>
-          <input
-            id="email"
-            className="input"
-            name="email"
-            type="text"
-            value=""
-            size="30"
-          />
-          <br />
-          <span id="email_validation" className="error_message"></span>
-        </div>
-        <div className="row">
-          <label className="required" htmlFor="message">
-            Your message:
-          </label>
-          <br />
-          <textarea
-            id="message"
-            className="input"
-            name="message"
-            rows="7"
-            cols="30"
-          ></textarea>
-          <br />
-          <span id="message_validation" className="error_message"></span>
-        </div>
-
-        <input id="submit_button" type="submit" value="Send email" />
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Tu nombre:</label>
+        <input type="text" name="user_name" />
+        <label>Tu email:</label>
+        <input type="email" name="user_email" />
+        <label>Dejanos tu consulta...</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
       </form>
     </section>
   );
