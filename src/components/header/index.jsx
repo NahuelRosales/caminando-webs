d9bddf313f2ff6d721309bc56665635de0b0ab99
Nahuel <nahuelfaridrosales@gmail.com>
2024-12-28 01:43:27 -0300

    fix bar menu in mobile version

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,60 +1,46 @@
 import { useState } from "react";
 import { Icon } from "@iconify/react";
-import Logo from "/logo.png"
+import Logo from "/logo.png";
 
 import "./style.scss";
 
 const Header = () => {
-    const [showMenuBar, setShowMenuBar] = useState(false);
+  const [showMenuBar, setShowMenuBar] = useState(false);
 
-    return (
-        <header className={`${showMenuBar ? "hiddenMenuBar" : "showMenuBar"}`}>
-            <Icon
-                className={`" icon " ${
-                    showMenuBar ? "hiddenMenuBar" : "showMenuBar"
-                }`}
-                icon="material-symbols-light:menu"
-                onClick={() => setShowMenuBar(!showMenuBar)}
-            />
-            {showMenuBar ? <div className="bg"></div> : null}
-
-            <nav className={` ${showMenuBar ? "menuBar" : "hiddenMenuBar"}`}>
-                <div
-                    className="top-menu-bar"
-                >
-                    <Icon
-                        className="icon close-menu"
-                        icon="material-symbols-light:close"
-                        onClick={() => setShowMenuBar(!showMenuBar)}
-                    />
-                    <p className="title">Hostel Backpackers</p>
-                </div>
-                <ul>
-                    <li>
-                        <Icon className="icon" icon="fluent:home-24-regular" />
-                        <p>Servicios</p>
-                    </li>
-                    <li>
-                        <Icon className="icon" icon="lucide:bed" />
-                        <p>Proyectos</p>
-                    </li>
-                    <li>
-                        <Icon className="icon" icon="bx:map" />
-                        <p>Nosotros</p>
-                    </li>
-                    <li>
-                        <Icon className="icon" icon="solar:gallery-broken" />
-                        <p>Contacto</p>
-                    </li>
-                    <li>
-                        <Icon className="icon" icon="solar:gallery-broken" />
-                        <p>Preguntas</p>
-                    </li>
-                </ul>
-            </nav>
-            <img src={Logo} alt="caminando-webs-logo" />
-        </header>
-    );
+  return (
+    <header className={`${showMenuBar ? "hiddenMenuBar" : "showMenuBar"}`}>
+      <Icon
+        className={`" icon " ${showMenuBar ? "hiddenMenuBar" : "showMenuBar"}`}
+        icon="material-symbols-light:menu"
+        onClick={() => setShowMenuBar(!showMenuBar)}
+      />
+      <nav className={` ${showMenuBar ? "menuBar" : "hiddenMenuBar"}`}>
+        <div className="top-menu-bar">
+          <Icon
+            className="icon close-menu"
+            icon="material-symbols-light:close"
+            onClick={() => setShowMenuBar(!showMenuBar)}
+          />
+          <img src={Logo} alt="caminando-webs-logo" />
+        </div>
+        <ul>
+          <li>
+            <p>Servicios</p>
+          </li>
+          <li>
+            <p>Proyectos</p>
+          </li>
+          <li>
+            <p>Nosotros</p>
+          </li>
+          <li>
+            <p>Contacto</p>
+          </li>
+        </ul>
+      </nav>
+      <img src={Logo} alt="caminando-webs-logo" />
+    </header>
+  );
 };
 
 export default Header;
